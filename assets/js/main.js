c946aea17fba968447e219801d6eed741211d948
Stephen Strange <80538114+Strange-bs@users.noreply.github.com>
2022-04-10 12:17:53 +0500

    added gallery images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // 'use strict';
-(function(){
-    document.querySelector('.nav-links').querySelectorAll('li').forEach( item => {
+(function () {
+    document.querySelector('.nav-links').querySelectorAll('li').forEach(item => {
         item.onmouseenter = function (event) {
             move.css({
                 opacity: 1,
@@ -15,23 +15,23 @@
         }
     })
 })();
-$(document).ready(function(){
+$(document).ready(function () {
     $("#courses .owl-carousel").owlCarousel({
         responsive: {
             0: {
-              items: 1,
+                items: 1,
             },
             1000: {
-              items: 3,
+                items: 3,
             }
-          },
-    reponsiveClass: true,
-      autoplayHoverPause: false,
-      autoplay: false,
-      smartSpeed: 1000,
-      dots: true,
-      center: true,
-      margin: 10
+        },
+        reponsiveClass: true,
+        autoplayHoverPause: false,
+        autoplay: false,
+        smartSpeed: 1000,
+        dots: true,
+        center: true,
+        margin: 10
     });
 
     $("#results .owl-carousel").owlCarousel({
@@ -39,40 +39,120 @@ $(document).ready(function(){
         items: 2,
         responsive: {
             0: {
-              items: 1,
+                items: 1,
             },
             1400: {
-              items: 2,
+                items: 2,
             },
             1600: {
                 items: 3
             }
-          },
+        },
     })
-  });
+});
 
 $('#teachers .owl-carousel').owlCarousel({
     margin: 40,
     dots: !0,
     responsive: {
         0: {
-          items: 1,
+            items: 1,
         },
         1400: {
-          items: 2,
+            items: 2,
         },
         1600: {
             items: 3
         }
-      },
+    },
 })
 
 
-$('.share .icon:first-child').on('mouseenter', function (e) {
+
+
+function checkSize() {
+    if (window.innerWidth < 768) {
+        $('.share .hidden').addClass('open')
+    } else {
+        $('.share .icon:first-child').on('mouseenter', function (e) {
     $(this)[0].parentElement.querySelector('.hidden').classList.add('open')
 })
 
 
 $('.share').on('mouseleave', function (e) {
     $('.share .hidden').removeClass('open')
+})
+    }
+}
+
+checkSize()
+
+$(window).on('resize', checkSize)
+
+document.querySelectorAll('dots').forEach(e => {
+    var cols = parseInt( e.getAttribute('cols'))
+    var rows = e.getAttribute('rows');
+    var dots = parseInt(e.getAttribute('dots'))
+    var color = e.getAttribute('bcolor');  
+    var x = e.getAttribute('x');
+    var y = e.getAttribute('y');
+    for (let i = 0 ; i < dots; i++) {
+        var dot = document.createElement('div');
+        dot.classList.add('dot');
+        dot.style.backgroundColor = color;
+        e.appendChild(dot);
+        e.css({
+            width: cols * 15 + 'px',
+            height: rows * 8 + 'px',
+        })
+    }   
+});
+
+// (function(){
+//     var pos =  [
+//         {
+//             x: 30,
+//             y: 10
+//         }, 
+//         {
+//             x: 70,
+//             y: 10
+//         },
+//         {
+//             x: 10,
+//             y: 50
+//         },
+//         {
+//             x: 90,
+//             y: 50
+//         },
+//         {
+//             x: 20,
+//             y: 90,
+//         },
+//         {
+//             x: 80,
+//             y: 90
+//         }
+//     ]
+//     var target = document.querySelectorAll('#about-us .absolute.img');
+//     console.log(target)
+//     target.forEach( (t, n) => {
+//         t.css({
+//             top: pos[n].y + '%',
+//             left: pos[n].x + '%'
+//         })
+//     })
+// })()
+
+$(document).ready(function () {
+    $('#faq .hidden').slideUp();
+    $('#faq .collapse[button]').click(function(e){
+        $(this).parent().parent().children().eq(1).slideToggle(200);
+        $(this).parent().parent().toggleClass('active');
+    })
+    $('#faq li:not(.active) .question').click(function(e){
+        $(this).parent().parent().children().eq(1).slideToggle(200);
+        $(this).parent().parent().toggleClass('active');
+    })
 })
